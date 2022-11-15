5c246f3bb3487625fab99aec64f68cf1491a02c9
Malkahiro <tahiro9011@gmail.com>
2022-11-15 14:06:30 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() =>{
+    const fetchPosts = async () =>{
+      const resp = await fetch('https://jsonplaceholder.typicode.com/todos');
+      const data = await resp.json();
+      console.log(data);
+      setPosts(data)
+    }
+
+    fetchPosts();
+  }, []);
+
   return (
     <div className="App">
       <p>Hello World</p>
+      {
+       posts.map((post) =>{
+         return <div key={post.id}>
+          <p>{post.title}</p>
+         </div>
+        })
+      }
     </div>
   );
 }
